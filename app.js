7d3b13128d85a451e1668b3738c2787a45de0a03
webhoster45 <gbengaayoolawale@gmail.com>
2026-06-12 23:12:57 +0100

    Force fix gitignore tracking

--- app.js
+++ app.js
@@ -5,14 +5,22 @@ const mongoose=require('mongoose');
 const jwt=require("jsonwebtoken");
 const bcrypt=require("bcrypt");
 const Schema=mongoose.Schema
-const PORT=process.env.PORT;
+const PORT=process.env.PORT||4000;
 const MONGODB_URL=process.env.MONGODB_URL
 const app=express();
 
-mongoose.connect("")
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose.connect(MONGODB_URL)
 .then(()=>{console.log("MongoDB connected✅"); app.listen(PORT,()=>console.log(`App running on PORT: ${PORT}`))})
 .catch(err=>console.log(err))
 
 const authmiddleware=async (req,res)=>{
-
+const authheader=req.headers.authorization;
+if(!authheader)return res.status(401).json({message:"Unauthorized"});
+const token=authheader.split(" ")[1];
+jwt.verify(token,process.env.JWT_SECRET,(err,decoded)=>{
+    if(err) return res.status(401).json({message:"unauthorized"})
+})
 }
